22cfe94c69a412102baa645735a8f620042d277a
jadeyliu <jadeliu999@gmail.com>
2025-03-26 17:41:41 -0400

    level progress

--- SE2250_SWE_Project/Assets/TutorialInfo/Scripts/Readme.cs
+++ SE2250_SWE_Project/Assets/TutorialInfo/Scripts/Readme.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿using System;
 using UnityEngine;
 
@@ -14,3 +15,20 @@ public class Readme : ScriptableObject
         public string heading, text, linkText, url;
     }
 }
+=======
+﻿using System;
+using UnityEngine;
+
+public class Readme : ScriptableObject {
+	public Texture2D icon;
+	public float iconMaxWidth = 128f;
+	public string title;
+	public Section[] sections;
+	public bool loadedLayout;
+	
+	[Serializable]
+	public class Section {
+		public string heading, text, linkText, url;
+	}
+}
+>>>>>>> Stashed changes
