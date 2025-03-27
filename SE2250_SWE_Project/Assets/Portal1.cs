3b493d0a64720f729906316f7155dc860a414a4f
RyanJeffery04 <RJeffer6@uwo.ca>
2025-03-26 21:24:21 -0400

    Portal Logic
    added some portal logic

---
+++ SE2250_SWE_Project/Assets/Portal1.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal1 : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(1); // Load Level 1
+        }
+    }
+}
