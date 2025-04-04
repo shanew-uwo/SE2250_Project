35e7eaed203c380985c67b51020f18d41ffe5d6d
Matthew Pachimala <mpachima@uwo.ca>
2025-04-04 13:29:24 -0400

    Added boss, arena and basic mechanics

---
+++ SE2250_SWE_Project/Assets/Scripts/Level 6/Health.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Health : MonoBehaviour
+{
+    public float maxHealth = 100f;
+    private float currentHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(float amount)
+    {
+        Debug.Log("Taking damage");
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        // Add logic for what happens when the entity dies
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
